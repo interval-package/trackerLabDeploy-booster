f5151a11be897507c0c536a259cfb83e7f67dfe8
zaterval <ziang_zheng@foxmail.com>
2025-09-07 09:39:17 +0800

    whc finish code

---
+++ scripts/test_whc.py
@@ -0,0 +1,44 @@
+import time
+from booster_robotics_sdk_python import (
+    ChannelFactory, RobotMode,
+)
+
+from tdeployBooster.core.controller.whole_body_controller import WholeBodyController
+
+Controller = WholeBodyController
+
+if __name__ == "__main__":
+    import argparse
+    import signal
+    import sys
+    import os
+
+    def signal_handler(sig, frame):
+        print("\nShutting down...")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default="temp", type=str, help="Name of the configuration file.")
+    parser.add_argument("--net", type=str, default="127.0.0.1", help="Network interface for SDK communication.")
+    args = parser.parse_args()
+    cfg_file = os.path.join("configs", args.config)
+
+    print(f"Starting custom controller, connecting to {args.net} ...")
+    ChannelFactory.Instance().Init(0, args.net)
+
+    with Controller(cfg_file) as controller:
+        time.sleep(2)  # Wait for channels to initialize
+        print("Initialization complete.")
+        controller.start_custom_mode_conditionally()
+        controller.start_rl_gait_conditionally()
+
+        try:
+            while controller.running:
+                controller.run()
+            controller.client.ChangeMode(RobotMode.kDamping)
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received. Cleaning up...")
+            controller.cleanup()
+            
---
+++ tdeployBooster/basic/motion_align/booster_k1.py
@@ -0,0 +1,18 @@
+K1_MOTION_ALIGN_CFG = {
+    "gym_joint_names": [
+        "AAHead_yaw:skip", "Head_pitch:skip",
+        "ALeft_Shoulder_Pitch", "Left_Shoulder_Roll", "Left_Elbow_Pitch", "Left_Elbow_Yaw", 
+        "ARight_Shoulder_Pitch", "Right_Shoulder_Roll", "Right_Elbow_Pitch", "Right_Elbow_Yaw",
+        "Left_Hip_Pitch", "Left_Hip_Roll", "Left_Hip_Yaw",
+        "Left_Knee_Pitch",
+        "Left_Ankle_Pitch:skip", "Left_Ankle_Roll:skip",
+        "Right_Hip_Pitch", "Right_Hip_Roll", "Right_Hip_Yaw",
+        "Right_Knee_Pitch",
+        "Right_Ankle_Pitch:skip", "Right_Ankle_Roll:skip"
+    ],
+    "dof_body_ids": [],
+    "dof_offsets": [22],
+    "invalid_dof_id": [],
+    "dof_indices_sim": [0],
+    "dof_indices_motion": [0]
+}
